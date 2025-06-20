8906867362c4d6e7fe10bf24388a89251046bfb2
LTKort <lars.kort94@gmail.com>
2025-06-20 16:29:59 +0200

    move functions

--- src/MarketingData.php
+++ src/MarketingData.php
@@ -2,6 +2,7 @@
 
 namespace Marshmallow\MarketingData;
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
 use Marshmallow\MarketingData\Casts\MarketingDataCast;
 use Marshmallow\MarketingData\Models\MarketingData as MarketingDataModel;
@@ -99,12 +100,119 @@ class MarketingData
         return $parameter_values_set;
     }
 
-    public static function getCookieValues(array $cookie_data): array
+    public static function setCookieValues($request, $session_key)
+    {
+        if (session()->has($session_key)) {
+            return;
+        }
+
+        /// WIP FOR ALL Cookies
+        $cookie_data = $request->cookie();
+        $cookie_values = self::getCookieValues($cookie_data);
+
+        if ($cookie_values && ! empty($cookie_values)) {
+            $request->session()->put($session_key, $cookie_values);
+        }
+
+        if (session()->has('mm_utm_values')) {
+            $session_data = session()->get('mm_utm_values');
+            $session_data['cookie_values'] = $cookie_values;
+            $request->session()->put('mm_utm_values', $session_data);
+        }
+    }
+
+    public static function getCookieValues($cookie_data): array
     {
         $cookie_keys = self::getMarketingDataCookies();
         $cookie_value_set = self::getValuesFromSet($cookie_keys, $cookie_data);
 
         return $cookie_value_set;
+    }
+
+    public static function setUtmValues($request, $session_key)
+    {
+        if (session()->has($session_key)) {
+            $session_data = session()->get($session_key);
+            $utm_source = Arr::get($session_data, 'utm_source', null);
+            $gclid = Arr::get($session_data, 'gclid', null);
+            $gclid_request = $request->input('gclid', null);
+            if ($gclid_request && $gclid_request == $gclid) {
+                return;
+            } elseif ($gclid || $session_data || $utm_source) {
+                return;
+            }
+        }
+
+        $request_data = $request->all();
+        $parameter_values_set = self::getRequestValues($request_data);
+
+        $parameter_values = collect($parameter_values_set)->mapWithKeys(function ($parameter_value, $parameter_key) use ($request) {
+
+            if ($parameter_key === 'landing_url') {
+                $parameter_value = $request->url();
+            }
+
+            if ($parameter_key === 'landing_path') {
+                $parameter_value = $request->path();
+                if (! Str::startsWith($parameter_value, '/')) {
+                    $parameter_value = '/' . $parameter_value;
+                }
+            }
+
+            if ($parameter_key === 'landing_full_url') {
+                $parameter_value = $request->fullUrl();
+            }
+
+            return [$parameter_key => $parameter_value];
+        })->reject(function ($parameter_value) {
+            return is_null($parameter_value);
+        })->toArray();
+
+        if ($parameter_values && ! empty($parameter_values)) {
+            $request->session()->put($session_key, $parameter_values);
+        }
+    }
+
+    public static function setSourceValues($request, $session_key)
+    {
+        if (! session()->has($session_key)) {
+            $intended_url = session()->get('url.intended');
+            $source_url = $request->server('HTTP_REFERER');
+
+            if (Str::of($source_url)->contains([
+                'livewire',
+                'oauth',
+                'password',
+                'reset',
+                'apple.com',
+            ]) && $intended_url) {
+                $source_url = $intended_url;
+            }
+
+            $source_path = null;
+            if ($source_url) {
+                $app_url = config('app.url');
+                $source_path = Str::of($source_url)
+                    ->before('?')
+                    ->after($app_url)
+                    ->toString();
+            }
+
+            $source_parameters = [
+                'source_url' => $source_url,
+                'source_path' => $source_path,
+            ];
+        } else {
+            $source_parameters = session()->get($session_key);
+        }
+
+        $source_parameters['previous_url'] = session()->get('_previous.url');
+        $source_parameters['request_url'] = $request->url();
+        $source_parameters['referer_url'] = $request->server('HTTP_REFERER');
+
+        if ($source_parameters && ! empty($source_parameters)) {
+            $request->session()->put($session_key, $source_parameters);
+        }
     }
 
     public static function getValuesFromSet(array $marketing_keys, array $data_set, bool $keep_empty_keys = false): array
@@ -132,6 +240,7 @@ class MarketingData
 
                 $marketing_key = Str::before($marketing_key, '*');
 
+
                 $matching_keys = collect($all_input_keys)->filter(function ($key) use ($marketing_key) {
                     return Str::startsWith($key, $marketing_key);
                 })->mapWithKeys(function ($matching_key) use ($data_set) {
@@ -150,6 +259,9 @@ class MarketingData
                 }
 
                 $matching_keys = $matching_keys->toArray();
+                if ($marketing_key == '_ga') {
+                    ray($all_input_keys, $marketing_key, $marketing_group_key, $data_set, $matching_keys);
+                }
 
                 if (empty($matching_keys)) {
                     return [];
--- src/Middleware/ParseMarketingParameters.php
+++ src/Middleware/ParseMarketingParameters.php
@@ -4,8 +4,6 @@ namespace Marshmallow\MarketingData\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Illuminate\Support\Arr;
-use Illuminate\Support\Str;
 use Marshmallow\MarketingData\Facades\MarketingDataTracker;
 
 class ParseMarketingParameters
@@ -30,120 +28,14 @@ class ParseMarketingParameters
         }
 
         // Set UTM values
-        $this->setUtmValues($request, 'mm_utm_values');
+        MarketingDataTracker::setUtmValues($request, 'mm_utm_values');
 
         // Set Source values
-        $this->setSourceValues($request, 'mm_source_values');
+        MarketingDataTracker::setSourceValues($request, 'mm_source_values');
 
         // Set Cookie values
-        $this->setCookieValues($request, 'mm_cookie_values');
+        MarketingDataTracker::setCookieValues($request, 'mm_cookie_values');
 
         return $next($request);
-    }
-
-    public function setCookieValues($request, $session_key)
-    {
-        if (session()->has($session_key)) {
-            return;
-        }
-
-        /// WIP FOR ALL Cookies
-
-        $cookie_values = MarketingDataTracker::getCookieValues($request->cookie());
-
-        if ($cookie_values && ! empty($cookie_values)) {
-            $request->session()->put($session_key, $cookie_values);
-        }
-
-        if (session()->has('mm_utm_values')) {
-            $session_data = session()->get('mm_utm_values');
-            $session_data['cookie_values'] = $cookie_values;
-            $request->session()->put('mm_utm_values', $session_data);
-        }
-    }
-
-    public function setUtmValues($request, $session_key)
-    {
-        if (session()->has($session_key)) {
-            $session_data = session()->get($session_key);
-            $utm_source = Arr::get($session_data, 'utm_source', null);
-            $gclid = Arr::get($session_data, 'gclid', null);
-            $gclid_request = $request->input('gclid', null);
-            if ($gclid_request && $gclid_request == $gclid) {
-                return;
-            } elseif ($gclid || $session_data || $utm_source) {
-                return;
-            }
-        }
-
-        $parameter_values_set = MarketingDataTracker::getRequestValues($request->all());
-
-        $parameter_values = collect($parameter_values_set)->mapWithKeys(function ($parameter_value, $parameter_key) use ($request) {
-
-            if ($parameter_key === 'landing_url') {
-                $parameter_value = $request->url();
-            }
-
-            if ($parameter_key === 'landing_path') {
-                $parameter_value = $request->path();
-                if (! Str::startsWith($parameter_value, '/')) {
-                    $parameter_value = '/' . $parameter_value;
-                }
-            }
-
-            if ($parameter_key === 'landing_full_url') {
-                $parameter_value = $request->fullUrl();
-            }
-
-            return [$parameter_key => $parameter_value];
-        })->reject(function ($parameter_value) {
-            return is_null($parameter_value);
-        })->toArray();
-
-        if ($parameter_values && ! empty($parameter_values)) {
-            $request->session()->put($session_key, $parameter_values);
-        }
-    }
-
-    public function setSourceValues($request, $session_key)
-    {
-        if (! session()->has($session_key)) {
-            $intended_url = session()->get('url.intended');
-            $source_url = $request->server('HTTP_REFERER');
-
-            if (Str::of($source_url)->contains([
-                'livewire',
-                'oauth',
-                'password',
-                'reset',
-                'apple.com',
-            ]) && $intended_url) {
-                $source_url = $intended_url;
-            }
-
-            $source_path = null;
-            if ($source_url) {
-                $app_url = config('app.url');
-                $source_path = Str::of($source_url)
-                    ->before('?')
-                    ->after($app_url)
-                    ->toString();
-            }
-
-            $source_parameters = [
-                'source_url' => $source_url,
-                'source_path' => $source_path,
-            ];
-        } else {
-            $source_parameters = session()->get($session_key);
-        }
-
-        $source_parameters['previous_url'] = session()->get('_previous.url');
-        $source_parameters['request_url'] = $request->url();
-        $source_parameters['referer_url'] = $request->server('HTTP_REFERER');
-
-        if ($source_parameters && ! empty($source_parameters)) {
-            $request->session()->put($session_key, $source_parameters);
-        }
     }
 }
